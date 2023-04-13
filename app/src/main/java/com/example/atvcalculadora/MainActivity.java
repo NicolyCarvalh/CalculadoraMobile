ab999e7333783923da9a006c1a22d5c8e6780ede
nicolycarvalh <nicolycarvalh15@gmail.com>
2023-04-13 12:04:13 -0300

    Declarando as variáveis

--- app/src/main/java/com/example/atvcalculadora/MainActivity.java
+++ app/src/main/java/com/example/atvcalculadora/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.atvcalculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvNumeros;
+    Button btZero, btUm, btDois, btTres, btQuatro, btCinco, btSeis, btSete, btOito, btNove, btPonto, btMultiplicacao, btSoma, btSubtracao, btResultado;
+    double numero = 0;
+    String num1 = " ", num2 = " ", calc="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
